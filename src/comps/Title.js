369706d8008a48e8167e139aa11ec31652d9e94d
Neha Sonpure <sonpureneha@gmail.com>
2020-08-24 21:31:11 +0530

    Update

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,8 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Artfolio</h1>
+      <h2>Share your art...</h2>
     </div>
   )
 }
